75ff96e5d20e11ef89004363d7b254ca53156666
calve <calvinh34@gmail.com>
2015-01-19 10:07:57 +0100

    New disk size

--- src/hardware_ini.h
+++ src/hardware_ini.h
@@ -1,7 +1,7 @@
-#define HDA_DATAREGS 	0x110        
+#define HDA_DATAREGS 	0x110
 #define HDA_CMDREG	0x3F6
 #define HDA_IRQ 	14
-#define HDA_MAXCYLINDER  16        
-#define HDA_MAXSECTOR    16           
-#define HDA_SECTORSIZE   256
-#define MAXVOL 8  
+#define HDA_MAXCYLINDER  1024
+#define HDA_MAXSECTOR    32
+#define HDA_SECTORSIZE   512
+#define MAXVOL 8
