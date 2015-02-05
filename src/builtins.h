5d00dfc2cfb44e135ecf1678574182b9366a3990
calve <calvinh34@gmail.com>
2015-02-05 20:26:47 +0100

    Complete builtins commands

--- src/builtins.h
+++ src/builtins.h
@@ -1,6 +1,12 @@
 #ifndef __BUILTINS_H__
 #define __BUILTINS_H__
 
+typedef struct {
+    char *name;                 /* User printable name of the function. */
+    void *func;                 /* Function to call to do the job. */
+    char *doc;                  /* Documentation for this function.  */
+} COMMAND;
+
 void do_help();
 void do_cat(char* arguments);
 void do_cd(char* arguments);
@@ -12,5 +18,21 @@ void do_ls(char* arguments);
 void do_mkdir(char* arguments);
 void do_mount(char* arguments);
 void do_rm(char* arguments);
+
+COMMAND commands[] = {
+  { "cat", do_cat, "Print file FILE" },
+  { "cd", do_cd, "Change to directory DIR" },
+  { "compute", do_cd, "Compute stuffs DIR" },
+  { "cp", do_cp, "cp source dest DIR" },
+  { "ed", do_ed, "edit FILE" },
+  { "help", do_help, "Display this text" },
+  { "?", do_help, "Synonym for `help'" },
+  { "ls", do_ls, "Synonym for `list'" },
+  { "mkdir", do_mkdir, "Make a directory" },
+  { "mount", do_mount, "Mount a volume" },
+  { "rm", do_rm, "Remove a volume" },
+  { "exit", do_exit, "Quit" },
+  { (char *)0, (void *)0, (char *)0 }
+};
 
 #endif
--- src/shell.c
+++ src/shell.c
@@ -15,7 +15,8 @@
 #include "mount.h"
 
 const char* shellsymbol = ">";
-
+char *command_generator PARAMS((const char *, int));
+char **shell_completion PARAMS((const char *, int, int));
 
 int construct_prompt(char* string, int string_size)
 {
@@ -28,6 +29,67 @@ int construct_prompt(char* string, int string_size)
   strcat(res,shellsymbol);
   strcpy(string,res);
   return 0;
+}
+
+
+/* Tell the GNU Readline library how to complete.  We want to try to complete
+   on command names if this is the first word in the line, or on filenames
+   if not. */
+void initialize_readline() {
+  /* Allow conditional parsing of the ~/.inputrc file. */
+  rl_readline_name = "ashell";
+
+  /* Tell the completer that we want a crack first. */
+  rl_attempted_completion_function = shell_completion;
+}
+
+/* Attempt to complete on the contents of TEXT.  START and END bound the
+   region of rl_line_buffer that contains the word to complete.  TEXT is
+   the word to complete.  We can use the entire contents of rl_line_buffer
+   in case we want to do some simple parsing.  Return the array of matches,
+   or NULL if there aren't any. */
+char** shell_completion (const char *text, int start, int end){
+  char **matches;
+
+  matches = (char **)NULL;
+
+  /* If this word is at the start of the line, then it is a command
+     to complete.  Otherwise it is the name of a file in the current
+     directory. */
+  if (start == 0)
+    matches = rl_completion_matches (text, command_generator);
+
+  return (matches);
+}
+
+/* Generator function for command completion.  STATE lets us know whether
+   to start from scratch; without any state (i.e. STATE == 0), then we
+   start at the top of the list. */
+char* command_generator (const char *text, int state)
+{
+       static int list_index, len;
+       char *name;
+
+       /* If this is a new word to complete, initialize now.  This includes
+          saving the length of TEXT for efficiency, and initializing the index
+          variable to 0. */
+       if (!state)
+         {
+           list_index = 0;
+           len = strlen (text);
+         }
+
+       /* Return the next name which partially matches from the command list. */
+       while ((name = commands[list_index].name))
+         {
+           list_index++;
+
+           if (strncmp (name, text, len) == 0)
+             return (strdup(name));
+         }
+
+       /* If no names matched, then return NULL. */
+       return ((char *)NULL);
 }
 
 static void empty_it(){ return; }
@@ -202,7 +264,7 @@ int main(int argc, char** argv){
   verbose("Boot successfull\n");
   mount_volume(0);
   verbose("Volume 0 has been automatically mounted. Use ``mount`` to mount another\n");
-
+  initialize_readline ();    /* Bind our completer. */
   printf("Welcome in shell. Build date %s %s\n", __DATE__, __TIME__);
   printf("Type ``help`` to find out all the available commands in this shell\n");
   printf("Add ``&`` in front of a command to run it in a new context (in background)\n");
