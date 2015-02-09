cf6208026d452544cc976eed43784f554f23c041
calve <calvinh34@gmail.com>
2015-02-09 12:18:34 +0100

    Fix semaphores for mutex

--- src/sem.c
+++ src/sem.c
@@ -17,16 +17,22 @@ void sem_down(struct sem_s *sem)
 {
   irq_disable();
   assert(sem->sem_magic_number==SEM_MAGIC_NUMBER);
-  sem->sem_compteur = sem->sem_compteur - 1;
-  if (sem->sem_compteur < 0)
+  if (sem->sem_compteur > 0)
   {
+    /* Take the semaphore and continue */
+    sem->sem_compteur = sem->sem_compteur - 1;
+    irq_enable();
+    return;
+  }
+  else {
+    /* Cannot take the semaphore now, give time to another process */
     current_ctx->state = BLOCKED;
     current_ctx->next_semaphore_context = sem->sem_ctx_blocked;
     sem->sem_ctx_blocked = current_ctx;
     irq_enable();
     yield();
+    return;
   }
-  irq_enable();
 }
 
 /* Fonction signal des semaphores */
@@ -36,7 +42,7 @@ void sem_up(struct sem_s *sem)
   irq_disable();
   assert(sem->sem_magic_number==SEM_MAGIC_NUMBER);
   sem->sem_compteur = sem->sem_compteur + 1;
-  if (sem->sem_compteur < 1)
+  if (sem->sem_compteur >= 1)
   {
     assert(sem->sem_ctx_blocked);
     ctx = sem->sem_ctx_blocked;
@@ -44,6 +50,6 @@ void sem_up(struct sem_s *sem)
     sem->sem_ctx_blocked = ctx->next_semaphore_context;
     ctx->next_semaphore_context=NULL;
   }
-  irq_enable();
+  yield();
 }
 
