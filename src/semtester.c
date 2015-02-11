44fca53e5805175df214dcd00a99fb0dfce5ae4e
calve <calvinh34@gmail.com>
2015-02-11 22:35:23 +0100

    semtester : multiple producers multiple consumers

--- src/semtester.c
+++ src/semtester.c
@@ -12,39 +12,63 @@ struct sem_s empty_count;
 int head, tail =0;
 int counter = 0;
 
-int producer(){
+void print_queue(int head, int tail){
+  printf("\t\t(");
+  for(int i = tail; i < head; i++){
+    printf("%i, ", i+1);
+  }
+  printf(")\n");
+}
+
+int producerA(){
     while(1) {
         sem_down(&empty_count);
         sem_down(&mutex);
         head++;
-        printf(" --> produced %i\n", head);
+        printf(" --> Alice produced %i", head);
+        print_queue(head, tail);
         sem_up(&mutex);
         sem_up(&fill_count);
         second_sleep(1);
     }
 }
 
-int consummerA(){
+int producerB(){
     while(1) {
-        sem_down(&fill_count);
+        sem_down(&empty_count);
         sem_down(&mutex);
-        tail++;
-        printf(" <-- consume-A %i\n", tail);
+        head++;
+        printf(" --> Bob produced %i", head);
+        print_queue(head, tail);
         sem_up(&mutex);
-        sem_up(&empty_count);
-        second_sleep(3);
+        sem_up(&fill_count);
+        second_sleep(1);
     }
 }
 
-int consummerB(){
+int consummerC(){
     while(1) {
         sem_down(&fill_count);
         sem_down(&mutex);
         tail++;
-        printf(" <-- consume-B %i\n", tail);
+        printf(" <-- Charlie consumed %i", tail);
+        print_queue(head, tail);
         sem_up(&mutex);
         sem_up(&empty_count);
-        second_sleep(5);
+        second_sleep(2);
+    }
+}
+
+int consummerD(){
+    while(1) {
+        sem_down(&fill_count);
+        sem_down(&mutex);
+        tail++;
+        printf(" <-- Dona consumed %i", tail);
+        print_queue(head, tail);
+        sem_up(&mutex);
+        sem_up(&empty_count);
+        second_sleep(1);
     }
 }
 
@@ -74,16 +98,17 @@ int main(){
   _out(TIMER_PARAM,128+64); /* reset + alarm on + 8 tick / alarm */
   _out(TIMER_ALARM,0xFFFFFFFD);  /* alarm at next tick (at 0xFFFFFFFF) */
   IRQVECTOR[TIMER_IRQ] = timer_it;
-  
+
   /* We are ready, begin to catch interruptions */
   _mask(1);
   printf("Binded timer interruptions\n");
   sem_init(&mutex, 1);
   sem_init(&fill_count, 0);
   sem_init(&empty_count, MAX_BUFFER);
-  create_ctx(16000, (void*) consummerA, (void*) NULL);
-  create_ctx(16000, (void*) consummerB, (void*) NULL);
-  create_ctx(16000, (void*) producer, (void*) NULL);
+  create_ctx(16000, (void*) consummerD, (void*) NULL);
+  create_ctx(16000, (void*) consummerC, (void*) NULL);
+  create_ctx(16000, (void*) producerA, (void*) NULL);
+  create_ctx(16000, (void*) producerB, (void*) NULL);
   yield();
   printf("hello world");
 }
